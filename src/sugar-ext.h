e3c5f74acdb79c00d19e9e347fc53de9cd0085d7
MostlyK <135974627+MostlyKIGuess@users.noreply.github.com>
2025-06-11 12:07:10 +0530

    chore: version and copyright updated
    - removed clangd and docs
    - updated copyright
    - version change and naming of library updated

--- src/sugar-ext.h
+++ src/sugar-ext.h
@@ -1,6 +1,6 @@
 /* sugar-ext.h
  *
- * Copyright 2025 MostlyK
+ * Copyright (C) 2025 MostlyK
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
